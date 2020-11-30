ae0d8810587d22429e5ad36349c0db4effbc6cd0
vitugo <vitorpacheco_14@hotmail.com>
2020-11-30 12:22:48 +0000

    Bootstrap

---
+++ app.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'json'
+
+require_relative 'automations.rb'
+
+before do
+  content_type :json
+end
+
+after do
+  response.body = JSON.dump(response.body)
+end
+
+get '/' do
+  { 'message': 'Hello world!' }
+end
+
+post '/events' do
+  Automation.new('create', message: 'Ola').trigger
+end
